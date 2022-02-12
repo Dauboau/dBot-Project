f3e1bfa2bdc69945d0e01d366658f0bdd15d9533
Daniel Contente Romanzini <86164187+Dauboau@users.noreply.github.com>
2022-02-12 14:46:40 +0000

    Ability to sum dices using a complex binary tree solution

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 import discord
 import numpy as np
+from AVL import *
 
 # Help(instructions) commands
 dhelp={'dregras','dRegras','dHelp','dhelp'}
+users_tree = AVLTree()
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -14,21 +16,111 @@ class MyClient(discord.Client):
         if message.content in dhelp:
           await message.channel.send(f"{message.author.name}, digite o dado que deseja jogar Ex: d12")
 
-        elif message.content[0]=="d":
+        elif len(message.content)>=2 and message.content[0]=="d":
           a=message.content
           a=a.replace('d','')
           try: # try transforming into an int
             a=int(a)
-            await message.channel.send(f"{np.random.randint(1,a+1)}")
+            rand=np.random.randint(1,a+1)
+            await message.channel.send(f"{rand}")
+
+            user_data=users_tree.search(message.author.name)
+            
+            if user_data==None:
+              users_tree.insert(message.author.name)
+              user_data=users_tree.search(message.author.name)
+              user_data.total_dice=rand
+            else:
+              user_data.total_dice=rand
+
           except: # if it fails,
             pass # it does not do anything
 
-        elif message.content[0]=="+":
-          b=message.content
-          b=b.replace('+','')
+        elif len(message.content)>=3 and message.content[0]=="+" and message.content[1]=="d":
+          a=message.content
+          a=a.replace('+','')
+          a=a.replace('d','')
           try:
+            a=int(a)
+            
+            rand=np.random.randint(1,a+1)
+
+            user_data=users_tree.search(message.author.name)
+
+            if user_data==None:
+              users_tree.insert(message.author.name)
+              user_data=users_tree.search(message.author.name)
+              user_data.total_dice=rand
+            else:
+              user_data.total_dice+=rand
+
+            await message.channel.send(f"{user_data.total_dice-rand}+{rand}={user_data.total_dice}")
+
+          except:
+            pass
+
+        elif len(message.content)>=3 and message.content[1]=="d":
+          try:
+            a=int(message.content[0])
+            b=message.content
+            aux=(message.content[0])
+            
+            b=b.replace(aux,'')
+            b=b.replace('d','')
             b=int(b)
-            await message.channel.send(f"{np.random.randint(1,b+1)+a}")
+
+            dices=[]
+
+            for i in range(a):
+              dices.append(np.random.randint(1,b+1))
+
+            total=sum(dices)
+
+            await message.channel.send(f"Os dados foram: {dices} com total: {total}")
+
+            user_data=users_tree.search(message.author.name)
+            
+            if user_data==None:
+              users_tree.insert(message.author.name)
+              user_data=users_tree.search(message.author.name)
+              user_data.total_dice=total
+            else:
+              user_data.total_dice=total
+
+          except:
+            pass
+
+        elif len(message.content)>=4 and message.content[0]=="+" and message.content[2]=="d":
+          try:
+            a=int(message.content[1])
+            b=message.content
+            aux=(message.content[1])
+            
+            b=b.replace('+','')
+            b=b.replace(aux,'')
+            b=b.replace('d','')
+            b=int(b)
+
+            print(a,b)
+
+            dices=[]
+
+            for i in range(a):
+              dices.append(np.random.randint(1,b+1))
+
+            total=sum(dices)
+
+            user_data=users_tree.search(message.author.name)
+
+            if user_data==None:
+              users_tree.insert(message.author.name)
+              user_data=users_tree.search(message.author.name)
+              user_data.total_dice=total
+            else:
+              user_data.total_dice+=total
+
+            await message.channel.send(f"{user_data.total_dice-total} + os dados: {dices} = {user_data.total_dice}")
+
           except:
             pass
 
